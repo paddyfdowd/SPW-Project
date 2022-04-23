706889532e78bf021c495d1cceb6f2fefeb8f41a
Paddy <paddydowd95@gmail.com>
2022-04-23 13:57:56 +0100

    cookie proof of concept

---
+++ SPW3/hyperspace/assets/js/cookie.js
@@ -0,0 +1,35 @@
+
+document.getElementById("setcookie").addEventListener("click", function(){setCookie("test",12345,20);}); 
+document.getElementById("getcookie").addEventListener("click", function(){getCookie("test");}); 
+
+
+function setCookie(cname, cvalue, exseconds){
+    alert(cname + cvalue + exseconds)
+    const d = new Date();
+    d.setTime(d.getTime() + (exseconds*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    alert(expires);
+}
+
+function getCookie(cname){
+
+
+    var allcookies = document.cookie;
+    alert(allcookies)
+    
+
+ /*    // Get all the cookies pairs in an array
+    cookiearray = allcookies.split(';');
+    
+    // Now take key value pair out of this array
+    for(var i=0; i<cookiearray.length; i++) {
+       name = cookiearray[i].split('=')[0];
+       value = cookiearray[i].split('=')[1];
+       document.write ("Key is : " + name + " and Value is : " + value);
+    } */
+  
+    
+    
+}
+
